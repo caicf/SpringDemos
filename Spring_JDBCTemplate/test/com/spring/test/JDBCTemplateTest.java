390a86027bfa305e1151cb39d64aa334a7935719
cai_cf <cai_cf@126.com>
2016-02-22 12:41:11 +0800

    规范代码

--- Spring_JDBCTemplate/test/com/spring/test/JDBCTemplateTest.java
+++ Spring_JDBCTemplate/test/com/spring/test/JDBCTemplateTest.java
@@ -1,19 +1,24 @@
 package com.spring.test;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 
 public class JDBCTemplateTest {
 
 	private ApplicationContext context;
+	private JdbcTemplate template;
 	
 	@Before
 	public void setUp() throws Exception {
 		context=new ClassPathXmlApplicationContext("applicationContext.xml");
+		template=(JdbcTemplate)context.getBean("jdbcTemplate");
 	}
 
 	@After
@@ -22,7 +27,7 @@ public class JDBCTemplateTest {
 
 	@Test
 	public void test() {
-		JdbcTemplate template=(JdbcTemplate)context.getBean("jdbcTemplate");
+		//创建caicf表
 		String sql="create table caicf(id int(11) , name varchar(20))";
 		template.execute(sql);
 	}
